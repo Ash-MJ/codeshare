340fcc00bce6786d9435f740f7fec60b2050b147
ashish <ashishparapuzha@gmail.com>
2020-09-16 20:13:26 +0530

    first commit

--- codeshare/models.py
+++ codeshare/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
 
 
     j_code = models.TextField()
+    
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
